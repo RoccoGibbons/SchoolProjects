b107b8181fbfa30ae69e87a485d20a5ba21e24a2
Rocco Gibbons <roccogibbons@outlook.com>
2024-09-25 11:04:35 +0100

    Added substitution cipher

--- cyphers/main.py
+++ cyphers/main.py
@@ -41,7 +41,7 @@ def csvCheck(cypher_type, output, key):
             return row["INPUT"]
     return False
 
-#This encrypts your message using a caesar cypher
+#This encrypts your message using a caesar cipher
 def caesarEncrypt():
     sentence = input("Please enter the sentence that you want to encrypt: ")
     key = randint(1, 25)
@@ -128,6 +128,100 @@ def caesarDecrypt():
             result += chr(letter)
         print(result)
 
+#This encrypts your message using a substitution cipher
+def substitutionEncrypt():
+    sentence = input("Please enter the sentence that you want to encrypt: ")
+
+    alphabet_dict = {'a': '', 'b': '', 'c': '', 'd': '', 'e': '', 'f': '', 'g': '', 'h': '', 'i': '', 'j': '', 'k': '', 'l': '', 'm': '', 'n': '', 
+                     'o': '', 'p': '', 'q': '', 'r': '', 's': '', 't': '', 'u': '', 'v': '', 'w': '', 'x': '', 'y': '', 'z': ''}
+    
+    key = ""
+    
+    alphabet_constant = "abcdefghijklmnopqrstuvwxyz"
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    #Create unique key and store it in a dictionary and a string
+    for i in range(len(alphabet_constant)):
+        position = randint(0, len(alphabet) - 1)
+        key += alphabet[position]
+        alphabet_dict[alphabet_constant[i]] = alphabet[position]
+        alphabet = alphabet[:position] + alphabet[position+1:]
+    
+    #binary number, if 1 then capital, if 0 then not capital
+    upper_case_test = ""
+    for letter in sentence:
+        if ord(letter) > 64 and ord(letter) < 91:
+            #is a capital letter
+            upper_case_test += "1"
+        else:
+            upper_case_test += "0"
+
+    sentence = sentence.lower()
+
+    encrypted = ""
+    for letter in sentence:
+        #is a letter
+        if ord(letter) > 96 and ord(letter) < 123:
+            encrypted += alphabet_dict[letter]
+        #is a symbol
+        else:
+            encrypted += letter
+    
+    result = ""
+    for i in range(len(encrypted)):
+        if upper_case_test[i] == "1":
+            result += encrypted[i].upper()
+        else:
+            result += encrypted[i]
+
+    print(f"Your encrypted message is {result}")
+    print(f"Your key is {key}")
+
+    csvAppend("substitution", sentence, result, key)
+
+#This decrypts your substitution cypher
+def substitutionDecrypt():
+    sentence = input("Please input your encrypted message: ")
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+
+    valid_key = False
+    while valid_key == False:
+        valid_key = True
+        key = input("Please enter your key: ")
+
+        for letter in key:
+            if letter in alphabet:
+                alphabet.replace(letter, '')
+                continue
+            else:
+                print("Key is not valid")
+                valid_key = False
+                break
+    
+    #check csv file
+    msg = csvCheck("substitution", sentence, key)
+
+    if msg != False:
+        print("The message was found in the database")
+        print(f"The decrypted message is: {msg}")
+        return
+    
+    else:
+        alphabet_dict = {}
+        alphabet_constant = "abcdefghijklmnopqrstuvwxyz"
+        
+        for i in range(len(key)):
+            alphabet_dict[key[i]] = alphabet_constant[i]
+        
+        result = ""
+        for letter in sentence:
+            result += alphabet_dict[letter]
+        
+        print("The message was not found in the database")
+        print(f"The decrypted message is {result}")
+        
+        return
+
 
 #---------------Beginning of main code---------------
 
@@ -153,38 +247,43 @@ while True:
     #Encryption
     if option == "1":
         print("What cypher do you want to use?")
-        cypher = input("Caesar: 1 add more here\n")
+        cypher = input("Caesar: 1 Substitution: 2\n")
 
         #Checks validity of user input
         if not cypher.isdigit():
             print("Please input a valid cypher")
             continue
 
-        if cypher not in ["1"]:
+        if cypher not in ["1", "2"]:
             print("Please input a valid cypher")
             continue
         
         #Runs the cyphers
         if cypher == "1":
             caesarEncrypt()
+        elif cypher == "2":
+            substitutionEncrypt()
+
 
     #Decryption
     elif option == "2":
         print("Which cypher are you decrypting?")
-        cypher = input("Caesar: 1 add more here\n")
+        cypher = input("Caesar: 1 Substitution: 2\n")
 
         #Checks validity of user input
         if not cypher.isdigit():
             print("Please input a valid cypher")
             continue
 
-        if cypher not in ["1"]:
+        if cypher not in ["1", "2"]:
             print("Please input a valid cypher")
             continue
         
         #Runs the decrypters
         if cypher == "1":
             caesarDecrypt()
+        elif cypher == "2":
+            substitutionDecrypt()
 
     #Exit loop
     elif option == "3":
---
+++ cyphers/test.py
@@ -0,0 +1,4 @@
+x = "abcdefg"
+pos = 2
+x = x[:pos] + x[pos+1:]
+print(x)
