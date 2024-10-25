3bfd6f72348b76430ad9d4e8200ed41f3f435e2a
rocco <roccogibbons@outlook.com>
2024-10-25 12:34:16 +0100

    progress on website - finished 'projects' page and starting to develop 'home' page

---
+++ website/script.js
@@ -0,0 +1,20 @@
+// vars q1 q2 q3 q4
+
+function quiz_score(q1, q2, q3, q4) {
+	total = 0;
+	
+	if(q1 == "answer here") {
+		total++;
+	}
+	if(q2 == "answer here") {
+		total++;
+	}
+	if(q3 == "answer here") {
+		total ++;
+	}
+	if q4 == "answer here") {
+		total ++;
+	}
+	return total;
+}
+
